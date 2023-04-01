0fd65742e7c5c58cf02f0813d8ea2a037061ae67
Balance.Is.Key <reimaginerei@gmail.com>
2023-04-01 10:17:49 -0400

    Fixed recusive function declaration for readability. Removed deprecated safedump and safe load functions. Flattening is working however output should show pretty. Leaving that as a todo

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,41 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as linter from 'yaml-lint';
 
+function processItem(processedData: any, item: any, aliases:any, key?: string) {
+	if (item && typeof item === 'object') {
+	  if (Array.isArray(item)) {
+		processedData[key || ''] = [];
+		item.forEach((value: any, index: number) => processItem(processedData, value, aliases, `${key || ''}[${index}]`));
+	  } else if (item['*']) {
+		aliases[item['*']] = { key, data: item };
+	  } else {
+		processedData[key || ''] = {};
+		Object.entries(item).forEach(([childKey, childValue]) => processItem(processedData, childValue, aliases, `${key ? `${key}.` : ''}${childKey}`));
+	  }
+	} else {
+	  processedData[key || ''] = item;
+	}
+  }
+
+function deconstructAliases(data: any) {
+	const processedData: any = {};
+	const aliases: any = {};
+	processItem(processedData,data, aliases);
+	Object.entries(aliases).forEach(([alias, { key, data }]) => {
+	  const value = processedData[alias];
+	  if (key && key.endsWith('[]')) {
+		const arrayKey = key.substring(0, key.length - 2);
+		processedData[arrayKey] = processedData[arrayKey] || [];
+		processedData[arrayKey].push(value);
+	  } else {
+		processedData[key || alias] = value;
+	  }
+	  delete data['*'];
+	  delete processedData[alias];
+	});
+	return processedData;
+  }
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -19,31 +54,31 @@ export function activate(context: vscode.ExtensionContext) {
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('yaml-flattener.helloWorld', () => {
 		const editor = vscode.window.activeTextEditor;
+		console.log('editor', editor);
 		if (editor) {
-      const document = editor.document;
-      const text = document.getText();
-      const options = { prettyErrors: true, schema: yaml.DEFAULT_SCHEMA };
-      const data = yaml.safeLoad(text, options);
-      // Perform any required transformations on the data structure
-      const formattedYaml = yaml.safeDump(data);
-      fs.writeFile(document.uri.fsPath, formattedYaml, (error) => {
-        if (error) {
-          vscode.window.showErrorMessage(`Failed to format YAML: ${error.message}`);
-        } else {
-          vscode.window.showInformationMessage('YAML formatted successfully!');
-        }
-      });
-      // Lint the YAML code and report any errors or warnings
-      linter.lint(text, (error, result) => {
-        if (error) {
-          vscode.window.showErrorMessage(`Failed to lint YAML: ${error.message}`);
-        } else if (result && result.errors && result.errors.length > 0) {
-          vscode.window.showWarningMessage(`YAML lint issues: ${result.errors.length}`);
-        } else {
-          vscode.window.showInformationMessage('YAML linted successfully!');
-        }
-      });
-    }
+			const document = editor.document;
+			const text = document.getText();
+			const options = { prettyErrors: true, schema: yaml.DEFAULT_SAFE_SCHEMA };
+			const data = yaml.load(text, options);
+			const processedData = deconstructAliases(data);
+			const formattedYaml = yaml.dump(processedData, options);
+			fs.writeFile(document.uri.fsPath, formattedYaml, (error) => {
+			  if (error) {
+				vscode.window.showErrorMessage(`Failed to format YAML: ${error.message}`);
+			  } else {
+				vscode.window.showInformationMessage('YAML formatted successfully!');
+			  }
+			});
+			linter.lint(text, (error, result) => {
+			  if (error) {
+				vscode.window.showErrorMessage(`Failed to lint YAML: ${error.message}`);
+			  } else if (result && result.errors && result.errors.length > 0) {
+				vscode.window.showWarningMessage(`YAML lint issues: ${result.errors.length}`);
+			  } else {
+				vscode.window.showInformationMessage('YAML linted successfully!');
+			  }
+			});
+		  }
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from yaml-flattener!');
